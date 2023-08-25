1ae2ffb071d532764ff6fc221f43a21db8ab2f57
BretMorrisGitHub <bret.morris0905@gmail.com>
2023-08-25 09:10:49 -0700

    C# live project commit

---
+++ TheatreCMS3/Scripts/Areas/Rent.js
@@ -0,0 +1,31 @@
+﻿var checkbox = $('.checkbox input[type="checkbox"]');
+var isChecked = checkbox.prop("checked");
+var label = $('.renthistory-damage');
+    if (isChecked) {
+        label.text('Damages Incurred');
+    } else {
+        label.text('Notes');
+    }
+
+$(document).ready(function () {
+    var checkbox = $('.checkbox input[type="checkbox"]');
+    var label = $('.renthistory-damage');
+
+    checkbox.on('change', function () {
+        var checked = this.checked;
+        if (checked) {
+            label.text('Damages Incurred');
+        } else {
+            label.text('Notes');
+        }
+    });
+});
+
+$(document).ready(function () {
+    $(".dropdown-button").hover(function () {
+        $(this).find(".dropdown-menu").show();
+    }, function () {
+        $(this).find(".dropdown-menu").hide();
+
+    });
+});
